b9f0df5f57d881427f544616dcb4d75711cc0fc4
Mika Tuupola <tuupola@appelsiini.net>
2018-09-03 18:26:38 +0300

    Initial commit

---
+++ copepod-hal.c
@@ -0,0 +1,63 @@
+/*
+
+Copyright (c) 2018 Mika Tuupola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include <gd.h>
+#include <color.h>
+
+#include "copepod-hal.h"
+
+static gdImagePtr img;
+FILE *png;
+
+/*
+ * Putpixel function. This is the only mandatory function which HAL
+ * must implement for copepod to be able to draw graphical primitives.
+ */
+void pod_hal_putpixel(int16_t x0, int16_t y0, uint16_t color)
+{
+    rgb_t rgb = rgb565_to_rgb888(&color);
+    int32_t gd_color = gdTrueColorAlpha(rgb.r, rgb.g, rgb.b, 0);
+    gdImageSetPixel(img, x0, y0, gd_color);
+}
+
+/*
+ * Initializes the GD HAL and set background color to black.
+ */
+void pod_hal_init(void)
+{
+    img = gdImageCreateTrueColor(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT);
+}
+
+/*
+ * Flushes image to a PNG file.
+ */
+void pod_hal_flush(void)
+{
+    png = fopen("test.png", "wb");
+    gdImagePng(img, png);
+    fclose(png);
+}
